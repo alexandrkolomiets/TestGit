8271ea142c88953fe5a2c480b837004adb09c427
Alex <alexandr.kolomiets.md@gmail.com>
2020-04-12 02:03:04 +0300

    Adding toast with new toast

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Test Text", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test Text version 2", Toast.LENGTH_SHORT).show();
     }
 }
